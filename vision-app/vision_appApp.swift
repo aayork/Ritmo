5f79a7a08be2f29e3eb9695295b76394b90e1b41
aayork <york_aa19@outlook.com>
2024-02-08 19:42:58 -0500

    work on immersive space

--- vision-app/vision_appApp.swift
+++ vision-app/vision_appApp.swift
@@ -12,7 +12,7 @@ struct vision_appApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-        }
+        }.windowStyle(.volumetric)
         
         ImmersiveSpace(id: "ImmersiveSpace") {
             ImmersiveView()
