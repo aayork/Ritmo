7f1280a831468b524eb398222ea505ad7a6c57b9
aayork <york_aa19@outlook.com>
2024-02-10 09:56:30 -0500

    New Views

---
+++ vision-app/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  vision-app
+//
+//  Created by Aidan York on 2/10/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
