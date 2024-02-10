af092d8daf195e8513fc9af3d54f7a61af5f0151
aayork <york_aa19@outlook.com>
2024-02-10 09:56:42 -0500

    New Views

--- vision-app/LibraryView.swift
+++ vision-app/LibraryView.swift
@@ -6,10 +6,116 @@
 //
 
 import SwiftUI
+import RealityKit
+import RealityKitContent
 
 struct LibraryView: View {
+    
+    @State private var playing = false
+        
+    @State var songTitle = "Not Playing"
+        
+    @State var artistName = "artist_name"
+
+    @State private var showImmersiveSpace = false
+    @State private var immersiveSpaceIsShown = false
+
+    @Environment(\.openImmersiveSpace) var openImmersiveSpace
+    @Environment(\.dismissImmersiveSpace) var dismissImmersiveSpace
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.clear
+            
+            Color(hue: 0.65, saturation: 0.9, brightness: 1.5, opacity: 0.3)
+            
+            Text("")
+                .ornament(
+                    visibility: .visible,
+                    attachmentAnchor: .scene(.bottom),
+                    contentAlignment: .center
+                ) {
+                    HStack {
+                        Button {
+                            if (playing == true) {
+                                pauseMusic();
+                                playing.toggle();
+                            }
+                            songTitle = cycleLeft()
+                            playMusic()
+                            playing = true
+                        } label: {
+                            Image(systemName: "backward.fill")
+                        }
+                        .buttonStyle(.borderless)
+                        .controlSize(.extraLarge)
+                        Button {
+                            if (playing == false) {
+                                playMusic();
+                            } else {
+                                pauseMusic();
+                            }
+                            playing.toggle()
+                        } label: {
+                            Image(systemName: playing ? "pause.fill" : "play.fill")
+                        }
+                        .buttonStyle(.borderless)
+                        .controlSize(.extraLarge)
+                        Button {
+                            if (playing == true) {
+                                pauseMusic();
+                                playing.toggle();
+                            }
+                            songTitle = cycleRight()
+                            playMusic()
+                            playing = true
+                        } label: {
+                            Image(systemName: "forward.fill")
+                        }
+                        .buttonStyle(.borderless)
+                        .controlSize(.extraLarge)
+                    }
+                    .labelStyle(.iconOnly)
+                    .padding(.vertical)
+                    .padding(.horizontal)
+                    .glassBackgroundEffect()
+                }
+            
+            
+            VStack {
+                
+                Text(songTitle)
+                    .font(.extraLargeTitle)
+                    .opacity(1.0)
+                    .padding(.horizontal, 50)
+                Text(artistName)
+                    .font(.subheadline)
+                    .opacity(0.5)
+                
+                Toggle("Show Immersive Space", isOn: $showImmersiveSpace)
+                    .toggleStyle(.button)
+                    .padding(.top, 50)
+            }
+            .padding()
+            .onChange(of: showImmersiveSpace) { _, newValue in
+                Task {
+                    if newValue {
+                        switch await openImmersiveSpace(id: "ImmersiveSpace") {
+                        case .opened:
+                            immersiveSpaceIsShown = true
+                        case .error, .userCancelled:
+                            fallthrough
+                        @unknown default:
+                            immersiveSpaceIsShown = false
+                            showImmersiveSpace = false
+                        }
+                    } else if immersiveSpaceIsShown {
+                        await dismissImmersiveSpace()
+                        immersiveSpaceIsShown = false
+                    }
+                }
+            }
+        }
     }
 }
 
