b320ece68ebae2795afbc9e9bb0ccfa16c19a26a
aayork <york_aa19@outlook.com>
2024-02-13 15:52:54 -0500

    pls help me fix this

--- vision-app/Views/ImmersiveView.swift
+++ vision-app/Views/ImmersiveView.swift
@@ -10,20 +10,25 @@ import RealityKit
 import RealityKitContent
 
 struct ImmersiveView: View {
-    @State private var audioController: AudioPlaybackController?
-    
-    var body: some View {
-        RealityView { content in
-            guard let entity = try? await Entity(named: "Immersive", in:
-                realityKitContentBundle) else {
-                fatalError("Unable to load scene model")
+   
+   @State private var playbackController: AudioPlaybackController?
+   
+   var body: some View {
+      RealityView { content in
+         // Add the initial RealityKit content
+         if let scene = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
+            //Accessing ambient emitter
+            let audioEntity = scene.findEntity(named: "AmbientAudioComponent")
+            // Add the audio source to a parent entity, and play a looping sound on it.
+            //example from Apple code to find the audio file res
+            if let audio = try? await AudioFileResource(named: "StarWars60",
+                                                        configuration: .init(shouldLoop: true)) {
+               playbackController = audioEntity?.prepareAudio(audio)
+               playbackController?.play()
             }
-            content.add(entity)
-        }
-    }
-}
-
-#Preview {
-    ImmersiveView()
+            content.add(scene)
+         }
+      }
+   }
 }
 
