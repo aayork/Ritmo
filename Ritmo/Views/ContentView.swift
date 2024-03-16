7d18aadbc3bce2b1ac02f081c31d0b2328658c9e
Aidan York <113198662+aayork@users.noreply.github.com>
2024-03-15 20:17:19 -0400

    Delete intermediate view

--- Ritmo/Views/ContentView.swift
+++ Ritmo/Views/ContentView.swift
@@ -12,6 +12,32 @@ import RealityKitContent
 struct ContentView: View {
     @Environment(GameModel.self) var gameModel
     var body: some View {
-        IntermediateView()
+        TabView {
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            MusicView()
+                .tabItem {
+                    Image(systemName: "music.note")
+                    Text("Play")
+                }
+            FriendsView()
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("Friends")
+                }
+            SettingsView()
+                .tabItem {
+                    Image(systemName: "gearshape")
+                    Text("Settings")
+                }
+        }
     }
+}
+
+#Preview {
+    ContentView()
+        .environment(GameModel().self)
 }
