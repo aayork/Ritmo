b88ccb28a42aa2d7e4743a2d0843dd877126ff94
Aidan York <113198662+aayork@users.noreply.github.com>
2024-03-19 16:05:15 -0400

    gray

--- Ritmo/Views/HomeView.swift
+++ Ritmo/Views/HomeView.swift
@@ -55,7 +55,7 @@ struct HomeView: View {
              style: .continuous
          )
         )
-        .background(Color.darkGray.opacity(0.8))
+        .background(Color.gunmetalGray.opacity(0.8))
     }
     
     private var contentStack: some View {
